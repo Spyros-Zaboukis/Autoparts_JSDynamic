fa84b12bf62f0df316398edc26306e17dd9a74ce
spyros zaboukis <spyros.zaboukis.83@gmail.com>
2022-12-21 10:38:19 +0100

    send refresh dom part

--- pieces.js
+++ pieces.js
@@ -1,51 +1,61 @@
 // Récupération des pièces depuis le fichier JSON
+//-------------------------------------------------
+
 const reponse = await fetch('pieces-autos.json');
 const pieces = await reponse.json();
 
-for (let i = 0; i < pieces.length; i++) {
 
-    const article = pieces[i];
+function genererPage(pieces){
 
-    // Récupération de l'élément du DOM qui accueillera les fiches
-    const sectionFiches = document.querySelector(".fiches");
-  
-  
-    // Création d’une balise dédiée à une pièce automobile
-    const pieceElement = document.createElement("article");
-   
-   
-    // Création des balises 
-    const imageElement = document.createElement("img");
-    imageElement.src = article.image;
-    const nomElement = document.createElement("h2");
-    nomElement.innerText = article.nom;
-    const prixElement = document.createElement("p");
-    prixElement.innerText = `Prix: ${article.prix} € (${article.prix < 35 ? "€" : "€€€"})`;
-    const categorieElement = document.createElement("p");
-    categorieElement.innerText = article.categorie ?? "(aucune catégorie)";
-    const descriptionElement = document.createElement("p");
-    descriptionElement.innerText = article.description ?? "Pas de description pour le moment.";
-    const stockElement = document.createElement("p");
-    stockElement.innerText = article.disponibilite ? "En stock" : "Rupture de stock";
-    
-    // On rattache la balise article a la section Fiches
-    sectionFiches.appendChild(pieceElement);
-   
-   
-    // On rattache l’image à pieceElement (la balise article)
-    pieceElement.appendChild(imageElement);
-    pieceElement.appendChild(nomElement);
-    pieceElement.appendChild(prixElement);
-    pieceElement.appendChild(categorieElement);
-    
-    
-    //Ajout des éléments au DOM pour l'exercice
-    pieceElement.appendChild(descriptionElement);
-    pieceElement.appendChild(stockElement);
+    for (let i = 0; i < pieces.length; i++) {
 
- }
+        const article = pieces[i];
+    
+        // Récupération de l'élément du DOM qui accueillera les fiches
+        const sectionFiches = document.querySelector(".fiches");
+      
+      
+        // Création d’une balise dédiée à une pièce automobile
+        const pieceElement = document.createElement("article");
+       
+       
+        // Création des balises 
+        const imageElement = document.createElement("img");
+        imageElement.src = article.image;
+        const nomElement = document.createElement("h2");
+        nomElement.innerText = article.nom;
+        const prixElement = document.createElement("p");
+        prixElement.innerText = `Prix: ${article.prix} € (${article.prix < 35 ? "€" : "€€€"})`;
+        const categorieElement = document.createElement("p");
+        categorieElement.innerText = article.categorie ?? "(aucune catégorie)";
+        const descriptionElement = document.createElement("p");
+        descriptionElement.innerText = article.description ?? "Pas de description pour le moment.";
+        const stockElement = document.createElement("p");
+        stockElement.innerText = article.disponibilite ? "En stock" : "Rupture de stock";
+        
+        // On rattache la balise article a la section Fiches
+        sectionFiches.appendChild(pieceElement);
+       
+       
+        // On rattache l’image à pieceElement (la balise article)
+        pieceElement.appendChild(imageElement);
+        pieceElement.appendChild(nomElement);
+        pieceElement.appendChild(prixElement);
+        pieceElement.appendChild(categorieElement);
+        
+        
+        //Ajout des éléments au DOM pour l'exercice
+        pieceElement.appendChild(descriptionElement);
+        pieceElement.appendChild(stockElement);
+    
+     }
+}
+
+
+genererPage(pieces);
  
-
+// Lister des éléments du dom
+//---------------------------------------
 
  // Function pour trier les résultats de sortie par ordre croissant :
 
@@ -59,7 +69,28 @@ for (let i = 0; i < pieces.length; i++) {
         return a.prix - b.prix;
     });
     console.log(piecesOrdonnes);
+    document.querySelector('.fiches').innerHTML='';
+    genererPage(piecesOrdonnes);
  });
+
+
+
+ // Function filter decroissant
+ const boutonFilterDecroissant = document.querySelector('.btn-trierDecroissant');
+
+ boutonFilterDecroissant.addEventListener('click', () => {
+     // Creation d'un array à partir d'un autre array
+     const piecesDecroissantes = Array.from(pieces);
+ 
+     piecesDecroissantes.sort((a,b)=>{
+         return b.prix - a.prix;
+     });
+     console.log(piecesDecroissantes);
+     document.querySelector('.fiches').innerHTML='';
+     genererPage(piecesDecroissantes);
+ 
+ });
+
 
 
  // Function pour filtrer les résultats en fonction du prix : 
@@ -71,35 +102,26 @@ for (let i = 0; i < pieces.length; i++) {
         return piece.prix <= 35;
     });
     console.log(piecesFiltrees);
+    document.querySelector('.fiches').innerHTML='';
+    genererPage(piecesFiltrees);
  });
 
 
- // Function filter decroissant
-const boutonFilterDecroissant = document.querySelector('.btn-trierDecroissant');
-
-boutonFilterDecroissant.addEventListener('click', () => {
-    // Creation d'un array à partir d'un autre array
-    const piecesDecroissantes = Array.from(pieces);
-
-    piecesDecroissantes.sort((a,b)=>{
-        return b.prix - a.prix;
-    });
-    console.log(piecesDecroissantes);
-
-});
-
-
-// Chap 4 Map liste :
-//--------------------
 
 // function filter par avis
 const bouttonFiltrerParAvis = document.querySelector('.filtrerParAvis');
 
 bouttonFiltrerParAvis.addEventListener('click' , () => { 
-    pieces.filter( (piece) =>{
-        return piece.description;
+   const piecesFiltresParAvis = pieces.filter( (piece) =>{
+        return piece.description != null;
     } );
+    console.log(piecesFiltresParAvis);
 });
+
+
+
+// Map liste :
+//--------------------
 
 const noms = pieces.map(piece => piece.nom);
 for(let i = pieces.length -1 ; i >= 0; i--){
@@ -108,6 +130,9 @@ for(let i = pieces.length -1 ; i >= 0; i--){
    };
 };
 console.log(noms);
+
+
+
 
 //Création de la liste
 const abordablesElements = document.createElement('ul');
@@ -140,3 +165,7 @@ pieces.map((piece) => {
         listeDePiecesDisponibles.appendChild(nomDePiece); 
     }
 });
+
+
+
+
